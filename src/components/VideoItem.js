906e80f226a1a45e7780dacb4befdef6155452d9
‘Juliano <julianoperins@gmail.com>
2020-05-04 18:35:28 -0400

    Added the videoDetail component

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,11 +1,13 @@
+import "./VideoItem.css";
 import React from "react";
 
-const VideoItem = props => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div>
-      {}
-      <img src={props.video.snippet.thumbnails.medium.url} />
-      {props.video.snippet.title}
+    <div onClick={() => onVideoSelect(video)} className="item video-item">
+      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
     </div>
   );
 };
